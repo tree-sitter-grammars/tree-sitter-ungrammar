3663c51257a6644e0c423ddb8d11b308aad00c1d
Amaan Qureshi <amaanq12@gmail.com>
2023-02-20 15:46:05 -0500

    feat(queries): update highlights, add injections, locals, indents, and folds

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,17 +1,28 @@
-(ERROR) @error
+(comment) @comment
 
-(line_comment) @comment
+(definition) @keyword
 
 (identifier) @variable
+
+(label_name) @label
 
 (token) @string
 
 [
   "="
   "|"
+] @operator
+
+[
+  "*"
+  "?"
+] @repeat
+
+[
   ":"
+] @punctuation.delimiter
+
+[
   "("
   ")"
-  "?"
-  "*"
-] @operator
+] @punctuation.bracket
